c0cbe37ae4d9d5880765c21ea8efa60f9619134f
Lakisha Trusty <daprincessanime@gmail.com>
2023-05-10 15:06:08 -0400

    finish adding the route element and nav with link

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Home from ".Pages/Home";
 import Index from ".Pages/Index";
 import New from ".Pages/New";
 import Show from ".Pages/Show";
+import About from ".Pages/About";
+import Nav from ".Pages/Nav";
 
 
 function App () {
@@ -13,14 +15,19 @@ function App () {
                 <div className="app-wapper">
                     <Nav/>
                     <Routes>
-                        <Route exact path="/" element={ <Home/>}/>
-                        <Route path="Show" element={ <Show/>}/>
-                        <Route/>
-                        <Route/>
-                        <Route/>
+                        <Route exact path="/" element={ <Home/> }/>
+                        <Route path="Show" element={ <Show/> }/>
+                        <Route path="Index" element={ <Index/> }/>
+                        <Route path="New" element={ <New/> }/>
+                        <Route path="About" element={ <About/>}/>
+                        <Route path="Edit" element={ <Edit/>}/>
                     </Routes>
                 </div>
             </Router>
         </div>
     )
 }
+
+
+
+export default App;
