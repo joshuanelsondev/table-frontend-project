e668dfc9064a4130694736c0e1f0db16cca920e6
joshuanelsondev <joshuanelsondev@gmail.com>
2023-05-11 12:35:46 -0400

    Edit index page

--- src/Components/Dishes.js
+++ src/Components/Dishes.js
@@ -20,7 +20,8 @@ export default function Dishes() {
 
     return (
         <div className="">
-            <div className="grid md:grid-cols-2 gap-y-8 m-8 w-full place-items-center sm:grid-cols-1"> 
+            <h1 className="text-3xl text-primary font-semibold m-8">My Table</h1>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-y-8 m-8 w-full place-items-center sm:grid-cols-1"> 
                 {dishes.map((dish) => {
                     return(
                     <Dish key={dish.id} dish={dish} />
