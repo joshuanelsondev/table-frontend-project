c2beb6be2c117eb048927ca3eb51e3082f51f10d
joshuanelsondev <joshuanelsondev@gmail.com>
2023-05-11 11:34:52 -0400

    Style the Index page

--- src/Components/Dish.js
+++ src/Components/Dish.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
 
 export default function Dish({ dish }) {
-    return (
-      <div className="bg-primary flex justify-center items-center rounded-full h-[450px] w-[450px]">
-        {/* <h1>{dish.name}</h1>
-        <h1>{dish.calories}</h1>
-        <h1>{dish.category}</h1>
-        <h1>{dish.portions}</h1>{" "} */}
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  }
+
+  return (
+    <div
+      className={`relative bg-primary flex justify-center items-center rounded-full h-[400px] w-[400px] ${
+        isFlipped ? "transform rotate-y-180" : ""
+      }`}
+    >
+      {!isFlipped && (
         <img
           src={dish.image_url}
           alt={dish.name}
-          className="h-[450px] w-[450px] rounded-full cursor-pointer  hover:outline outline-8 outline-white"
+          className="absolute h-[200px] w-auto rounded cursor-pointer hover:outline outline-4 outline-white"
+          onClick={handleClick}
         />
-      </div>
-    );
+      )}
+
+      {isFlipped && (
+        <div className="absolute flex flex-col items-center gap-8">
+          <h2 className="font-bold text-xl text-white">{dish.name}</h2>
+          <div className="text-white font-semibold">
+            <p>{dish.category}</p>
+            <p>
+              <span className="text-secondary font-bold">Calories: </span>
+              {dish.calories}
+            </p>
+            <p>
+              <span className="text-secondary font-bold">Portions: </span>
+              {dish.portions}
+            </p>
+          </div>
+          <button onClick={handleClick} className="bg-secondary rounded-3xl p-2 text-sm text-primary font-bold">View Image</button>
+        </div>
+      )}
+    </div>
+  );
 }
