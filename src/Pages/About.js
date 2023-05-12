70e31a3373a6e93e17afbc09202132fc58b74d80
Lakisha Trusty <daprincessanime@gmail.com>
2023-05-12 12:59:26 -0400

    could not pull from main without changes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ export default function About() {
     return (
         <div>
           <h1>The Table</h1>
-            <a>It's a celebration of coming together and enjoying each company through food. This application allow you to create a Table in a potluck or dinner party setting.   </a>  
+            <p>It's a celebration of coming together and enjoying each company through food. This application allow you to create a Table in a potluck or dinner party setting.   </p>  
         </div>
     )
 }
