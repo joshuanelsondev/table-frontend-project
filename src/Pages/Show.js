a9f2a98eb3f20da55ed91edcd886c05e59a050c0
joshuanelsondev <joshuanelsondev@gmail.com>
2023-05-13 15:53:29 -0400

    Edit Dish.js

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+
 export default function Show() {
-  return <div></div>;
+  const [dish, setDish] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`${API}/dishes/${id}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, [id]);
+
+  return (
+    <div>
+
+    </div>
+  )
 }
